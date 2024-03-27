db1f771a9d714a7a5252d003e691f6de5bc90bf4
lord-cosmos <teslaatozinspires7@gmail.com>
2024-03-27 16:28:14 -0400

    fix nosql-injection threat

--- pages/create_book.js
+++ pages/create_book.js
@@ -1,13 +1,23 @@
-let Book = require('../models/book');
-let Author = require('../models/author');
-let Genre = require('../models/genre');
+let Book = require("../models/book");
+let Author = require("../models/author");
+let Genre = require("../models/genre");
 
 function getAuthor(family_name, first_name) {
-  return Author.findOne({family_name: family_name, first_name: first_name});
+  family_name = sanitizeInput(family_name);
+  first_name = sanitizeInput(first_name);
+  return Author.findOne({ family_name: family_name, first_name: first_name });
+}
+
+function sanitizeInput(name) {
+  if (typeof name !== string) {
+    return null;
+  }
+  return name.replace(/[^\w\s]/gi, "");
 }
 
 function getGenre(name) {
-  return Genre.find({name: name});
+  name = sanitizeInput(name);
+  return Genre.find({ name: name });
 }
 
 exports.new_book = async (res, family_name, first_name, genre_name, title) => {
@@ -15,11 +25,11 @@ exports.new_book = async (res, family_name, first_name, genre_name, title) => {
   let genre = await getGenre(genre_name).exec();
   let book = Book({
     title: title,
-    summary: 'Demo Summary to be updated later',
+    summary: "Demo Summary to be updated later",
     author: author,
-    isbn: 'ISBN2022',
-    genre: genre
+    isbn: "ISBN2022",
+    genre: genre,
   });
   await book.save();
-  res.send('Created new book : ' + book);
-}
+  res.send("Created new book : " + book);
+};
