f0e6a13f5d041bb55be00644763b5d8050e9e579
lord-cosmos <teslaatozinspires7@gmail.com>
2024-03-27 16:22:50 -0400

    fix nosql-injection threat

--- pages/book_details.js
+++ pages/book_details.js
@@ -1,22 +1,32 @@
-let async = require('async');
-let Book = require('../models/book');
-let BookInstance = require('../models/bookinstance');
+let async = require("async");
+let Book = require("../models/book");
+let BookInstance = require("../models/bookinstance");
 
 function get_book(id) {
-    if (typeof id !== "string") {
-        return ({status: "error"});
-    }
-    return Book.findOne({'_id': {$eq: id}}).populate('author');
+  if (typeof id !== "string") {
+    return { status: "error" };
+  }
+  return Book.findOne({ _id: { $eq: id } }).populate("author");
 }
 
 function get_book_dtl(id) {
-  return BookInstance
-          .find({ 'book': id })
-          .select('imprint status');
+  // Validate input
+  if (typeof id !== "string" || id.trim() === "") {
+    return { status: "error", message: "Invalid input" };
+  }
+
+  // Sanitize input to prevent NoSQL injection
+  const sanitizedId = id.trim(); // Assuming trimming is sufficient for your use case
+
+  // Perform query using sanitized input
+  return BookInstance.find({ book: sanitizedId }).select("imprint status");
 }
 
 exports.show_book_dtls = async (res, id) => {
-  const results = await Promise.all([get_book(id).exec(), get_book_dtl(id).exec()])
+  const results = await Promise.all([
+    get_book(id).exec(),
+    get_book_dtl(id).exec(),
+  ]);
   try {
     let book = await results[0];
     let copies = await results[1];
@@ -25,8 +35,7 @@ exports.show_book_dtls = async (res, id) => {
       author: book.author.name,
       copies: copies,
     });
-  }
-  catch(err) {
+  } catch (err) {
     res.send(`Book ${id} not found`);
-  } 
-}
+  }
+};
